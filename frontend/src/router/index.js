c8d09fa79bdd5820642fdcb9de17f144d064877a
Maxson <msmmj6499@gmail.com>
2023-05-17 13:26:38 +1000

    Initial frontend commit

---
+++ frontend/src/router/index.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes: [
+		{
+			path: '/',
+			component: Home
+		},
+		{
+			path: '/',
+			component: () => import('../views/Scenario1.vue')
+		},
+	],
+})
+
+export default router
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+<body>
+<main id="Home-page">
+	<h1>COMP90024: Cluster and Cloud Computing</h1><br><hr>
+	<br><br><h2>Assignment 2</h2>
+	<br><br><h2>Team 46:</h2><br>
+	<h3>Kunal</h3>
+	<h3>Mayank</h3>
+	<h3>Sophie</h3>
+	<h3>Harsh</h3>
+	<h3>Maxson</h3>
+	</main>
+</body>
+</template>
+<style lang="scss" scoped>
+ body{
+    background-image:url(b2.gif);
+	background-repeat: no-repeat;
+    background-size: cover;
+	width: 1500px;
+ } 
+  h1{
+	text-align: center;
+	font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+	color: white;
+	
+
+}
+h2{
+	text-align: center;
+	font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+	color: white;
+
+}
+h3{
+	text-align: center;
+	font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+	color: white;
+}
+</style>
