9f6a85dad0f2c6e921c62957e912d7f3a74b6ff9
Kunal Patel <kunal.patel0999@gmail.com>
2023-05-20 00:33:45 +1000

    added profanity views

---
+++ createviews/profanityplace.py
@@ -0,0 +1,28 @@
+import logging
+import couchdb
+dbname='tweetsprofain'
+dbaddress='http://admin:Royai99@127.0.0.1:5984/'
+couch=couchdb.Server(dbaddress)
+db=couch[dbname]
+
+def createView( dbConn, designDoc, viewName, mapFunction ):
+    data = {
+            "_id": f"_design/{designDoc}",
+            "views": {
+                viewName: {
+                    "map": mapFunction,
+                    "reduce":"_stats"
+                    }
+            },
+            "language": "javascript",
+            "options": {"partitioned": False }
+            }
+    logging.info( f"creating view {designDoc}/{viewName}" )
+    dbConn.save( data )
+
+mapFunction = '''function (doc) {
+  if(doc.place.full_name){
+    emit(doc.place.full_name,doc.profanity);
+  }
+}'''
+createView( db, "placeprofvalue", "new-view", mapFunction )
