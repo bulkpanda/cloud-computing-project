3a4a3b965a1b5681d9857100ba9925e300fff526
Kunal Patel <kunal.patel0999@gmail.com>
2023-05-21 00:45:18 +1000

    batch upload to the database

--- createprofanitydb.py
+++ createprofanitydb.py
@@ -1,48 +1,33 @@
 import couchdb
 import json
-from profanity_check import predict, predict_prob
+import time
+
+filename='/home/ubuntu/tweetdata/twitter-profane.json'
+jsonfile=open(filename,'r', encoding='utf-8')
 dbname='tweetsprofane'
 dbaddress='http://admin:Royai99@127.0.0.1:5984/'
-# dbaddress='http://admin:Royai99@172.26.134.130:5984/' #change the address to the couchdb server
 couch = couchdb.Server(dbaddress)
 if dbname in couch:
     del couch[dbname]
     db=couch.create(dbname)
 else:
     db=couch.create(dbname)
-filename='twitter-place-data.json'
-filename='C:/Users/Kunal Patel/D folder/_Master_data_science/Cluster and Cloud Computing/twitter-place-data.json'
-filename='\home\ubuntu\tweetdata\tweet-place-data.json'
-jsonfile=open(filename,'r', encoding='utf-8')
 i=-1
+data_array=[]
 for row in jsonfile:
     i+=1
-    if(i%100==0):
-        # print(row[:-2])
-        try:
-            data = json.loads(row[:-2])
-            
-        except:
-            print(f'This row can\'t be converted: {row}')
-        else:
-            try:
-                place=data['doc']['includes']['places'][0]
-            except:
-                # print(data['doc']['includes']['places'])
-                place={'full_name':'Australia'}
-            time=data['doc']['data']['created_at']
-            text=data['doc']['data']['text']
-            sentiment=data['doc']['data']['sentiment']
-            # tags=data['doc']['data']['entities']['mentions']
-            tokens=data['value']['tokens']
-            profanity=predict_prob([text])[0]
-            # print(text, profanity)
-            db_entry={
-                'place':place,
-                'time':time,
-                'text':text,
-                'sentiment':sentiment,
-                'tokens':tokens,
-                'profanity':profanity
-            }
-            db.save(db_entry)
+    try:
+        data = json.loads(row[:-2])
+        
+    except:
+        print(f'This row can\'t be converted: {row}')
+    else:
+        data_array.append(data)
+        
+    if(len(data_array)==1000):
+        print(i)
+        db.update(data_array)
+        data_array=[]
+
+if len(data_array)!=0:
+    db.update(data_array)
